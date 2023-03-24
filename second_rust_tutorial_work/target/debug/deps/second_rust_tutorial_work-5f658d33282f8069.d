a103275b75a2285121ac594ce8610eac38d54d49
duhongpeng <18638675345@163.com>
2023-03-24 11:44:32 +0800

    test

---
+++ second_rust_tutorial_work/target/debug/deps/second_rust_tutorial_work-5f658d33282f8069.d
@@ -0,0 +1,5 @@
+/home/duhongpeng/rust/rust_tutorial/second_rust_tutorial_work/target/debug/deps/second_rust_tutorial_work-5f658d33282f8069: src/main.rs
+
+/home/duhongpeng/rust/rust_tutorial/second_rust_tutorial_work/target/debug/deps/second_rust_tutorial_work-5f658d33282f8069.d: src/main.rs
+
+src/main.rs:
---
+++ second_rust_tutorial_work/target/debug/second_rust_tutorial_work.d
@@ -0,0 +1,1 @@
+/home/duhongpeng/rust/rust_tutorial/second_rust_tutorial_work/target/debug/second_rust_tutorial_work: /home/duhongpeng/rust/rust_tutorial/second_rust_tutorial_work/src/main.rs
